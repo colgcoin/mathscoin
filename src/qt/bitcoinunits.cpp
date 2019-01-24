053a99add95ef1a304f45f1bd18a2a63f06d1c01
colgcoin <colgcoin@gmail.com>
2019-01-24 19:30:40 +0000

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("CLG");
+    case mBTC: return QString("mCLG");
+    case uBTC: return QString::fromUtf8("μCLG");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("MathsCoin");
+    case mBTC: return QString("Milli-MathsCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-MathsCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
